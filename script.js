3ca77f14df3ffeaeb739283ca9de519d93c9fe24
ProgramminguyFromScratch <ianjeremiahwatson@gmail.com>
2025-04-08 19:13:58 -0500

    e

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const bestGames = [
   {href: "SuperHot", text: "Super Hot", image:"thumbnails/superhot.jpg"},
   {href: "timeshooter1", text: "Time Shooter 1", image:"thumbnails/timeshooter1.jpg"},
   {href: "blockblast", text: "Block Blast", image:"thumbnails/blockblast.jpg"},
-  {href: "scratch/Appel.html", text: "Appel", image:"thumbnails/appel.png"},
+  {href: "appel", text: "Appel", image:"thumbnails/appel.png"},
   {href: "scratch/Snail.html", text: "Snail Platformer", image:"thumbnails/Snail.png"},
   {href: "scratch/SMM4.html", text: "Super Mario Maker 4", image:"thumbnails/SMM4.png"},
 ]
